7145fea78d2f64a55d7c840b327eafdf331f45bc
Tomislav Bozinovic <tomislav.bozinovic93@gmail.com>
2021-04-07 18:36:08 +0200

    Vjezba5

---
+++ LV5/nmap.py
@@ -0,0 +1,33 @@
+import socket
+import datetime
+from local_machine_info import print_machine_info
+sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print('Vrijeme pokretanja programa: ' , datetime.datetime.now())
+print('Program se izvodi na ovom računalu: ')
+print_machine_info()
+
+print('--------------------------------------------------------------')
+
+hostAddress = input('Molimo unesite adresu hosta: ')
+ipAddress = socket.gethostbyname(hostAddress)
+print ('Skeniram host: ', hostAddress, ' IP adresa: ', ipAddress)
+
+portNum1 = int(input('Početni port >> '))
+portNum2 = int(input('Završni port >> '))
+
+def scanner(port):
+    try:
+        sk.connect((hostAddress, port))
+        return True
+    except:
+        return False
+
+for portNumber in range(portNum1,portNum2):
+    print("Skeniram port: ", portNumber)
+    if scanner(portNumber):
+        print('Port', portNumber, 'je otvoren')
+
+print ('Skeniranje portova završeno!!!')
+
+# https://linuxhint.com/python-for-hacking-port-scanner/
